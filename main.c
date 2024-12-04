4ca104903d20ed24e6c86b4b00461aed443ede85
Hridesh MG <hridesh699@gmail.com>
2024-12-05 04:31:58 +0530

    feat: finish video renderer

--- main.c
+++ main.c
@@ -1,4 +1,8 @@
+#include <libavcodec/avcodec.h>
+#include <libavformat/avformat.h> //handle formats
+#include <libswscale/swscale.h>   //scaling and converting pixel formats
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/ioctl.h>
 #include <time.h>
@@ -13,6 +17,12 @@
 
 #define ASCII_CHARS " .`-_':;,^\\\"~|<>(){}[]1!il?+*xjrvctzunowmYXJLCOQ0Z@#&%$"
 
+struct video_data {
+  char **ascii_frames; // Array of ascii strings
+  int frame_count;
+  double frame_rate;
+};
+
 struct frame {
   unsigned char *data;
   int height;
@@ -26,31 +36,135 @@ struct winsize get_window_size() {
   return w;
 }
 
-void load_image(struct frame *current_frame) {
-  int width, height, channels;
-  unsigned char *data = stbi_load("eva.jpg", &width, &height, &channels, 3);
-  if (data == NULL) {
-    printf("Error in loading image!\n");
-    exit(1);
-  }
-  printf("Loaded image with width %dpx, height %dpx\n", width, height);
+// function declarations
+char *convert_frame_to_ascii(struct frame *frame);
+int get_intensity(uint8_t *pixel_ptr);
+struct winsize get_window_size();
+void adapt_frame(struct frame *frame);
 
+char *convert_frame_to_ascii(struct frame *frame) {
+  size_t image_size = frame->height * frame->width * 3;
+  size_t string_size = frame->height * frame->width + frame->height;
+  char *ascii_string = (char *)malloc(string_size + 1);
+  ascii_string[string_size] = '\0';
+
+  unsigned char *input_ptr = frame->data;
+  char *ascii_ptr = ascii_string;
+
+  int pixel_count = 1;
+  while (input_ptr != frame->data + image_size) {
+    int intensity = get_intensity(input_ptr);
+    *ascii_ptr = ASCII_CHARS[(int)(intensity / 255.0 * (strlen(ASCII_CHARS) - 1))];
+
+    if (pixel_count % frame->width == 0) {
+      *(ascii_ptr + 1) = '\n';
+      ascii_ptr++;
+    }
+
+    pixel_count++;
+    ascii_ptr++;
+    input_ptr += 3; // the pixel data is in the format - [R1, G1, B1, R2, G2, B3] where each pixel is (R1, G1, B1)
+  }
+  return ascii_string;
+}
+
+void load_frames(struct video_data *video_data) {
+  char *input_filename = "video.mp4";
+  AVFormatContext *format_ctx = NULL;
+  avformat_open_input(&format_ctx, input_filename, NULL, NULL); // automatically detects the format, no special options
+  avformat_find_stream_info(format_ctx, NULL);                  // info about audio and video streams
+
+  int video_stream_index = -1;            // to tell that no video stream is found when initialized
+  AVCodecParameters *codec_params = NULL; // structure to store info about encoded data
+  for (unsigned int i = 0; i < format_ctx->nb_streams; i++) {
+    if (format_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+      video_stream_index = i;
+      codec_params = format_ctx->streams[i]->codecpar;
+      break;
+    }
+  }
+
+  const AVCodec *codec =
+      avcodec_find_decoder(codec_params->codec_id);          // find the codec based on id to decode video frames
+  AVCodecContext *codec_ctx = avcodec_alloc_context3(codec); // codec context which tells how to decode the video
+  avcodec_parameters_to_context(codec_ctx, codec_params);    // copy from ctx to params
+  avcodec_open2(codec_ctx, codec, NULL);                     // opens the codec to decode
+
+  int total_frames = 0;
+  AVRational frame_rate = format_ctx->streams[video_stream_index]->avg_frame_rate;
+  int64_t duration = format_ctx->duration; // duration in AV_TIME_BASE (microseconds)
+
+  if (frame_rate.num > 0 && frame_rate.den > 0) {
+    double duration_in_seconds = (double)duration / AV_TIME_BASE; // converting to seconds
+    total_frames =
+        (int)(duration_in_seconds * (double)frame_rate.num /
+              (double)frame_rate.den); // num = no. of frames in a time period, den = time period in which frames occur
+  } else {
+    total_frames = (int)(duration / AV_TIME_BASE * 30);
+  }
+  video_data->frame_rate = (double)frame_rate.num / frame_rate.den;
+
+  struct frame current_frame;
+  int frame_count = 0;
+  video_data->ascii_frames = malloc(sizeof(char *) * total_frames);
+  char **ascii_frame_ptr = video_data->ascii_frames; // pointer to first element of array
+  AVFrame *frame = av_frame_alloc();
+  AVPacket *packet = av_packet_alloc();
+
+  while (av_read_frame(format_ctx, packet) >= 0) {
+    if (packet->stream_index == video_stream_index) {
+      avcodec_send_packet(codec_ctx, packet);
+      while (avcodec_receive_frame(codec_ctx, frame) == 0) {
+        struct SwsContext *sws_ctx = sws_getContext(frame->width, frame->height, codec_ctx->pix_fmt, frame->width,
+                                                    frame->height, AV_PIX_FMT_RGB24, SWS_BILINEAR, NULL, NULL, NULL);
+
+        uint8_t *rgb_data = malloc(frame->width * frame->height * 3); // allocate memory for rgb data of frame
+        sws_scale(sws_ctx, (const uint8_t *const *)frame->data, frame->linesize, 0, frame->height, &rgb_data,
+                  (int[]){frame->width * 3}); // convert the frame to rgb format using context from earlier
+
+        current_frame.data = rgb_data;
+        current_frame.width = frame->width;
+        current_frame.height = frame->height;
+        adapt_frame(&current_frame);
+
+        *ascii_frame_ptr = convert_frame_to_ascii(&current_frame);
+        printf("Finished processing %d frames\r", frame_count);
+
+        frame_count++;
+        ascii_frame_ptr++;
+        free(current_frame.data);
+        sws_freeContext(sws_ctx);
+        break;
+      }
+    }
+    av_packet_unref(packet);
+  }
+  printf("\n");
+  video_data->frame_count = frame_count;
+
+  av_packet_free(&packet);
+  av_frame_free(&frame);
+  avcodec_free_context(&codec_ctx);
+  avformat_close_input(&format_ctx);
+}
+
+void adapt_frame(struct frame *frame) {
   // Resize image to terminal dimensions
   struct winsize w = get_window_size();
   int desired_width = w.ws_col;
   // int desired_height = desired_width / ((float)width / height); //(Correct aspect ratio calculation)
   int desired_height = w.ws_row;
-  current_frame->data = malloc(desired_width * desired_height * 3);
-  printf("Resized width: %d, height: %d\n", desired_width, desired_height);
 
-  stbir_resize_uint8_srgb(data, width, height, width * 3, current_frame->data, desired_width, desired_height,
-                          desired_width * 3, STBIR_RGB);
-  current_frame->width = desired_width;
-  current_frame->height = desired_height;
-  stbi_image_free(data);
+  uint8_t *resized_data = malloc(desired_width * desired_height * 3);
+  stbir_resize_uint8_srgb(frame->data, frame->width, frame->height, frame->width * 3, resized_data, desired_width,
+                          desired_height, desired_width * 3, STBIR_RGB);
+  free(frame->data);
+  frame->data = resized_data;
+  frame->width = desired_width;
+  frame->height = desired_height;
 }
 
-int get_intensity(unsigned char *pixel_ptr) {
+int get_intensity(uint8_t *pixel_ptr) {
   unsigned char r = *pixel_ptr;
   unsigned char g = *(pixel_ptr + 1);
   unsigned char b = *(pixel_ptr + 2);
@@ -61,46 +175,28 @@ int get_intensity(unsigned char *pixel_ptr) {
 }
 
 int main() {
-  struct frame current_frame;
-  load_image(&current_frame);
+  struct video_data video_data;
+  char *ascii_frame_ptr;
+  load_frames(&video_data);
 
-  float startTime = (float)clock() / CLOCKS_PER_SEC; // This returns  processor seconds, it
-                                                     // doesn't translate to the real world seconds
-  float endTime = (float)clock() / CLOCKS_PER_SEC;
-  if (BENCHMARK)
-    printf("Loading the image took %f seconds\n", endTime - startTime);
+  for (int i = 0; i < video_data.frame_count; i++) {
+    printf("%s", video_data.ascii_frames[i]);
+    double time_for_each_frame = 1000 / video_data.frame_rate; // in milliseconds
 
-  size_t image_size = current_frame.height * current_frame.width * 3;
-  size_t string_size = current_frame.height * current_frame.width +
-                       current_frame.height; // +current_frame.height for accounting newlines
+    usleep(time_for_each_frame * 1000); // usleep expects microseconds
 
-  char *ascii_string = (char *)malloc(string_size + 1);
-  ascii_string[string_size] = '\0';
-
-  unsigned char *input_ptr = current_frame.data;
-  char *ascii_ptr = ascii_string;
-
-  startTime = (float)clock() / CLOCKS_PER_SEC;
-  int pixel_count = 1;
-  while (input_ptr != current_frame.data + image_size) {
-    int intensity = get_intensity(input_ptr);
-    *ascii_ptr = ASCII_CHARS[(int)(intensity / 255.0 * (strlen(ASCII_CHARS) - 1))];
-
-    if (pixel_count % current_frame.width == 0) {
-      *(ascii_ptr + 1) = '\n';
-      ascii_ptr++;
+    int newline_count = 0;
+    ascii_frame_ptr = video_data.ascii_frames[i];
+    while (*ascii_frame_ptr != '\0') {
+      if (*ascii_frame_ptr == '\n') {
+        newline_count++;
+      }
+      ascii_frame_ptr++;
+    }
+    for (int i = 0; i < newline_count; i++) {
+      printf("\033[A\33[2K\r"); // clear previous line.
     }
 
-    pixel_count++;
-    ascii_ptr++;
-    input_ptr += 3; // the pixel data is in the format - [R1, G1, B1, R2, G2, B3] where each pixel is (R1, G1, B1)
+    free(video_data.ascii_frames[i]);
   }
-  endTime = (float)clock() / CLOCKS_PER_SEC;
-  if (BENCHMARK)
-    printf("Converting the image took %f seconds\n", endTime - startTime);
-
-  printf("%s", ascii_string);
-
-  free(ascii_string);
-  stbi_image_free(current_frame.data);
 }
